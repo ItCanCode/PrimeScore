262030313c43fc201528a4cd632280796750575b
LesibaKgokolo1 <2726968@students.wits.ac.za>
2025-08-07 23:03:54 +0200

    Add Codecov bundler plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react-swc'
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,12 @@ export default defineConfig({
     eslint({
       include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
       exclude: ['node_modules', 'dist'],
+    }),
+    
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "<bundle project name>",
+      uploadToken: process.env.CODECOV_TOKEN,
     }),
   ],
 })
