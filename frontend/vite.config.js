3511068c1fb0089ce866fe75b0bf796a8d719f7c
Jan Moloto <janmoloto8@gmail.com>
2025-08-07 15:51:47 +0200

    Linter

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import eslint from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslint({
+      include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+      exclude: ['node_modules', 'dist'],
+    }),
+  ],
 })
