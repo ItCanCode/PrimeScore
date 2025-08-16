e40078a812f314055e6a13a80e06434adaae68a6
basi743 <basicodes743@gmail.com>
2025-08-16 18:02:51 +0200

    modified userController.js

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,3 +1,6 @@
+import admin from "../config/firebaseAdmin.js";
+
+const db = admin.firestore();
 
 // controllers/userController.js
 let users = [{ id: 1, name: 'John Doe' }];
@@ -13,4 +16,44 @@ export const createUser = (req, res) => {
   };
   users.push(newUser);
   res.status(201).json(newUser);
+};
+
+export const getCurrentUser = async (req, res) => {
+  try {
+    const idToken = req.headers.authorization?.split("Bearer ")[1];
+    if (!idToken) return res.status(401).json({ message: "No token" });
+
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    const uid = decodedToken.uid;
+
+    const userDoc = await db.collection("users").doc(uid).get();
+    if (!userDoc.exists) return res.status(404).json({ message: "User not found" });
+
+    res.json({ user: userDoc.data() });
+  } 
+  catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to fetch user" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const { username, bio, picture } = req.body;
+    const userId = req.user.uid; 
+
+    const userRef = db.collection('users').doc(userId);
+
+    await userRef.update({
+      "profile.username": username,
+      "profile.bio": bio,
+      "profile.picture": picture,
+    });
+
+    const updatedUser = await userRef.get();
+    res.json({ message: "Profile updated", user: updatedUser.data() });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to update profile" });
+  }
 };
