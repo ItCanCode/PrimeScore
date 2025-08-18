6d1509c669fd57599dbdf7725e5eaea414964d5c
strangerhm3@gmail.com <strangerhm3@gmail.com>
2025-08-19 01:05:06 +0200

    getMatches api

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,14 +1,13 @@
 // routes/userRoutes.js
 
 import express from 'express';
-import { getAllUsers, createUser, getCurrentUser, updateUser } from '../controllers/userController.js';
+import { getAllUsers, createUser, getCurrentUser, updateUser,getMatches } from '../controllers/userController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
-
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.post('/', createUser);
 router.get('/me', authMiddleware ,getCurrentUser);
 router.put('/me', authMiddleware, updateUser);
-
+router.get('/viewMatches',getMatches)
 export default router;
