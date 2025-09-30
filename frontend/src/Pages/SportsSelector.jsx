f81ce193fe3491b9e8603c53c7d438e6884c1a73
LesibaKgokolo1 <2726968@students.wits.ac.za>
2025-09-30 11:39:53 +0200

    add Frontends Tests(Homepage, PastMatches, SportSelector, TeamManagement

--- frontend/src/Pages/SportsSelector.jsx
+++ frontend/src/Pages/SportsSelector.jsx
@@ -1,4 +1,4 @@
-import { useState ,useEffect,useContext} from 'react';
+import React, { useState ,useEffect,useContext} from 'react';
 import { useNavigate,Link  } from 'react-router-dom';
 import { ChevronRight, Home, Trophy, Users, Calendar, X } from 'lucide-react';
 import LeagueModal from '../Components/leagueModal.jsx';
