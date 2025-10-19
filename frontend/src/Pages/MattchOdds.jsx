31c2920fe85ff148db3c4b3a28aead1ed4d18ba9
strangerhm3@gmail.com <strangerhm3@gmail.com>
2025-10-19 16:28:58 +0200

    add things in the .env

--- frontend/src/Pages/MattchOdds.jsx
+++ frontend/src/Pages/MattchOdds.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import '../Styles/RugbyFixtures.css';
 const MatchOdds = () => {
   const [matches, setMatches] = useState([]);
   const [loading, setLoading] = useState(true);
